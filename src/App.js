c5e4dd7b6a69affd9e53d35868f0e71d7c1b722b
Vicky <kosenchiha@gmail.com>
2020-04-06 11:55:08 +0100

    Display answers with questions on main page with form inputs

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import data from './data/dummy.json';
 const App = () => {
   return (
     <div>
-      <ol>
-        {data.map((question) => {
-          return <div>
-            <li key={question.id}>{question.text}</li>
-            <ul>{question.answers.map((answer) => {
-              return <li key={answer.id}>{answer.text}</li>
-            })}
-            </ul>
-          </div>
-        })}
-      </ol>
+      <form>
+        <ol>
+          {data.map(question => {
+            return (
+              <div>
+                <li key={question.id}>{question.text}</li>
+
+                {question.answers.map(answer => {
+                  return (
+                    <div>
+                      <input
+                        type="radio"
+                        id={`${question.id}.${answer.id}`}
+                        name={question.id}
+                        value={answer.id}
+                      />
+                      <label for={`${question.id}.${answer.id}`}>
+                        {answer.text}
+                      </label>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </ol>
+      </form>
     </div>
   );
 };
