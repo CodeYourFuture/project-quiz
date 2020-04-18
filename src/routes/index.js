1dcfcae7c57c0bd353bf7485f1abfd11a311e115
Vicky <kosenchiha@gmail.com>
2020-04-18 15:58:25 +0100

    Remove empty lines in routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home';
-
 import About from '../components/About';
 import Quiz from '../components/Quiz';
 
