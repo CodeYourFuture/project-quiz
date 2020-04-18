e76499194a0cacbf5bfd634285cc3845bcc7757a
Vicky <kosenchiha@gmail.com>
2020-04-18 14:18:13 +0100

    Create routes for Quiz, Home, About components

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../components/Home';
+
+import About from '../components/About';
+import Quiz from '../components/Quiz';
+
+export default () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/quiz" component={Quiz} />
+    <Route path="/about" component={About} />
+  </Switch>
+);
