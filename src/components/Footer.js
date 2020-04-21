e70d032b4791b3ab7e73e24dc4ed3d48ecdab25b
abdulkadiret <ethiogentelman@gmail.com>
2020-04-21 19:58:58 +0100

    Remove custome.scss & move it's content into App.scss & index.scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import './custom.scss';
 
 const Footer = () => {
   const currentYear = () => {
