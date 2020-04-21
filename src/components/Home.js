072b20cbeef214541df717e7c56023609283db49
abdulkadiret <ethiogentelman@gmail.com>
2020-04-21 20:27:03 +0100

    Add a footer component for every page via react router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Footer from './Footer';
 
 const Home = () => {
   return (
-    <div id="container">
-      <div id="main">
-        <h1> Hello This is Home page</h1>
-      </div>
-      <Footer />
+    <div>
+      <h1> Hello This is Home page</h1>
     </div>
   );
 };
