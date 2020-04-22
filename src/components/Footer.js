3aced7196ea937ac309552dffcaf49a24a9bff56
abdulkadiret <ethiogentelman@gmail.com>
2020-04-22 09:57:17 +0100

    Use react bootstrap components & update the footer UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Container, Col, Row } from 'react-bootstrap';
 
 const Footer = () => {
   const currentYear = () => {
@@ -10,93 +11,116 @@ const Footer = () => {
   };
 
   return (
-    <footer className="font-small blue bg-primary pt-2">
-      <div className="container-fluid text-center text-md-left">
-        <span className="row d-flex justify-content-center">
-          <div className="col-md-5 mt-md-0 mt-0 d-flex justify-content-center">
-            <p className="text-white mb-1 mt-1 pr-1">
-              You can find the source code on Github
-            </p>
-            <a
-              className="github social"
-              href="https://github.com/CodeYourFuture/project-quiz/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
-          </div>
-          <hr className="clearfix d-md-none" />
+    <footer className="bg-primary pt-3">
+      <Container fluid>
+        <Row>
+          <Col xs={6} className="pr-4 pl-4 border-right border-secondary">
+            <h6 className="text-center text-uppercase text-white mb-0">
+              Social
+            </h6>
+            <hr className="mb-1 mt-1" style={{ width: '80px' }} />
+            <Row className="d-flex justify-content-center">
+              <ul className="list-unstyled mb-1">
+                <li className="text-center">
+                  <p className="mt-0 mb-1">
+                    <a
+                      className="github social"
+                      href="https://github.com/CodeYourFuture/project-quiz/"
+                      target="_blank"
+                    >
+                      <FontAwesomeIcon
+                        aria-hidden="false"
+                        aria-label="Github icon"
+                        icon={faGithub}
+                      />
+                      <span class="ml-2"> Github </span>
+                    </a>
+                  </p>
+                </li>
+              </ul>
+            </Row>
+          </Col>
 
-          <div className="col-md-6 mt-md-0 mt-1">
-            <h6 className="text-center text-uppercase text-white">
+          <Col xs={6} className="pr-5 pl-2 border-right">
+            <h6 className="text-center text-uppercase text-white mb-0">
               Developers
             </h6>
-            <hr className="clearfix" style={{ width: '100px' }} />
-
-            <div className="container-fluid text-center text-md-left">
-              <span className="row">
-                <div className="col-md-3 col-6 text-center">
-                  <h6 className="text-white mt-1 mb-0">Vicky</h6>
-                  <ul className="list-unstyled mb-0">
-                    <li className="text-center">
-                      <a
-                        className="linkedin social"
-                        href="https://www.linkedin.com/in/viktoriia-kosenko-030983168/"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                      </a>
+            <hr className="mb-1 mt-1" style={{ width: '110px' }} />
+            <Row className="mt-md-0 mt-1">
+              <Col md={12}>
+                <Row className="d-flex justify-content-center">
+                  <ul className="list-unstyled mb-1">
+                    <li>
+                      <p className="mt-0 mb-1">
+                        <a
+                          className="linkedin social"
+                          href="https://www.linkedin.com/in/viktoriia-kosenko-030983168/"
+                          target="_blank"
+                        >
+                          <FontAwesomeIcon
+                            aria-hidden="false"
+                            aria-label="Linkedin icon"
+                            icon={faLinkedin}
+                            size="1x"
+                          />
+                          <span class="ml-2"> Vicky </span>
+                        </a>
+                      </p>
+                    </li>
+                    <li>
+                      <p className="mt-0 mb-1">
+                        <a
+                          className="linkedin social"
+                          href="https://www.linkedin.com/mwlite/in/ahmed8merehil"
+                          target="_blank"
+                        >
+                          <FontAwesomeIcon
+                            aria-hidden="false"
+                            aria-label="Linkedin icon"
+                            icon={faLinkedin}
+                            size="1x"
+                          />
+                          <span class="ml-2"> Ahmed </span>
+                        </a>
+                      </p>
+                    </li>
+                    <li>
+                      <p className="mt-0 mb-1">
+                        <a
+                          className="linkedin social"
+                          href="https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/"
+                          target="_blank"
+                        >
+                          <FontAwesomeIcon
+                            aria-hidden="false"
+                            aria-label="Linkedin icon"
+                            icon={faLinkedin}
+                            size="1x"
+                          />
+                          <span class="ml-2"> Abdulkadir </span>
+                        </a>
+                      </p>
+                    </li>
+                    <li>
+                      <p className="mt-0 mb-1">
+                        <a className="linkedin social" href="#" target="_blank">
+                          <FontAwesomeIcon
+                            aria-hidden="false"
+                            aria-label="Linkedin icon"
+                            icon={faLinkedin}
+                            size="1x"
+                          />
+                          <span class="ml-2"> Ali </span>
+                        </a>
+                      </p>
                     </li>
                   </ul>
-                </div>
-
-                <div className="col-md-3 col-6 text-center">
-                  <h6 className="text-white mt-1 mb-0">Ahmed</h6>
-                  <ul className="list-unstyled mb-0">
-                    <li className="text-center">
-                      <a
-                        className="linkedin social"
-                        href="https://www.linkedin.com/mwlite/in/ahmed8merehil"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="col-md-3 col-6 text-center">
-                  {/* <!-- Links --> */}
-                  <h6 className="text-white mt-1 mb-0">Ali</h6>
-                  <ul className="list-unstyled mb-0">
-                    <li className="text-center">
-                      <a className="linkedin social" href="#" target="_blank">
-                        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="col-md-3 col-6 text-center">
-                  <h6 className="text-white mt-1 mb-0">Abdulkadir</h6>
-                  <ul className="list-unstyled mb-0">
-                    <li className="text-center">
-                      <a
-                        className="linkedin social"
-                        href="https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </span>
-            </div>
-          </div>
-        </span>
-      </div>
-
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
       <div className="bg-info text-white text-center py-2">
         <small>
           Copyright &copy; {`${currentYear()}`}, CYF Scot Quiz Project
