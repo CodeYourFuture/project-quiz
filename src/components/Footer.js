ac326cdb9e79d16f935edb2f5e9fc43cb03ea51d
abdulkadiret <ethiogentelman@gmail.com>
2020-04-23 16:31:11 +0100

    Change class name and remove unnecessary codes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,7 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
   const currentYear = () => {
     const today = new Date();
-    const year = today.getFullYear();
-    return year;
+    return today.getFullYear();
   };
 
   return (
@@ -14,112 +13,102 @@ const Footer = () => {
       <div className="d-flex justify-content-around">
         <section>
           <h6 className="text-uppercase">Social</h6>
-          <ul className="list-unstyled mb-1">
+          <ul className="list-unstyled">
             <li>
-              <p className="mb-0">
-                <a
-                  className="github social text-white"
-                  href="https://github.com/CodeYourFuture/project-quiz/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon
-                    aria-hidden="true"
-                    aria-label="Github icon"
-                    icon={faGithub}
-                  />
-                  <span class="ml-2"> Github </span>
-                </a>
-              </p>
+              <a
+                className="footer-link text-white"
+                href="https://github.com/CodeYourFuture/project-quiz/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  className="mr-2"
+                  aria-hidden="true"
+                  aria-label="Github icon"
+                  icon={faGithub}
+                />
+                Github
+              </a>
             </li>
           </ul>
         </section>
 
         <section>
           <h6 className="text-uppercase">Developers</h6>
-          <ul className="list-unstyled mb-1">
+          <ul className="list-unstyled">
             <li>
-              <p className="mb-0">
-                <a
-                  className="linkedin social text-white"
-                  href="https://www.linkedin.com/in/viktoriia-kosenko-030983168/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon
-                    aria-hidden="true"
-                    aria-label="Linkedin icon"
-                    icon={faLinkedin}
-                    size="1x"
-                  />
-                  <span class="ml-2"> Vicky </span>
-                </a>
-              </p>
+              <a
+                className="footer-link text-white"
+                href="https://www.linkedin.com/in/viktoriia-kosenko-030983168/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  className="mr-2"
+                  aria-hidden="true"
+                  icon={faLinkedin}
+                  size="1x"
+                />
+                Vicky
+              </a>
             </li>
             <li>
-              <p className="mb-0">
-                <a
-                  className="linkedin social text-white"
-                  href="https://www.linkedin.com/mwlite/in/ahmed8merehil"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon
-                    aria-hidden="true"
-                    aria-label="Linkedin icon"
-                    icon={faLinkedin}
-                    size="1x"
-                  />
-                  <span class="ml-2"> Ahmed </span>
-                </a>
-              </p>
+              <a
+                className="footer-link text-white"
+                href="https://www.linkedin.com/mwlite/in/ahmed8merehil"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  className="mr-2"
+                  aria-hidden="true"
+                  icon={faLinkedin}
+                  size="1x"
+                />
+                Ahmed
+              </a>
             </li>
             <li>
-              <p className="mb-0">
-                <a
-                  className="linkedin social text-white"
-                  href="https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon
-                    aria-hidden="true"
-                    aria-label="Linkedin icon"
-                    icon={faLinkedin}
-                    size="1x"
-                  />
-                  <span class="ml-2"> Abdulkadir </span>
-                </a>
-              </p>
+              <a
+                className="footer-link text-white"
+                href="https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  className="mr-2"
+                  aria-hidden="true"
+                  icon={faLinkedin}
+                  size="1x"
+                />
+                Abdulkadir
+              </a>
             </li>
             <li>
-              <p className="mb-0">
-                <a
-                  className="linkedin social text-white"
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon
-                    aria-hidden="true"
-                    aria-label="Linkedin icon"
-                    icon={faLinkedin}
-                    size="1x"
-                  />
-                  <span class="ml-2"> Ali </span>
-                </a>
-              </p>
+              <a
+                className="footer-link text-white"
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  className="mr-2"
+                  aria-hidden="true"
+                  icon={faLinkedin}
+                  size="1x"
+                />
+                Ali
+              </a>
             </li>
           </ul>
         </section>
       </div>
-
       <section className="bg-info text-center py-2">
-        <small>
-          <p className="mb-0">
-            Copyright &copy; {`${currentYear()}`}, CYF Scot Quiz Project
-          </p>
-        </small>
+        <p className="mb-0">
+          <small>
+            Copyright &copy; {`${currentYear()}`}, CYF Scot Quiz Project{' '}
+          </small>
+        </p>
       </section>
     </footer>
   );
