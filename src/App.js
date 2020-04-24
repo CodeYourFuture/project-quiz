c4e102eb4b60f27d0548b7cd220f1139d7a31e2c
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-04-24 18:17:03 +0100

    Edit home page with text, and links to Quiz page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import Home from './components/Home';
 import './App.scss';
 
 const App = () => {
   return (
     <Router>
-      <Routes />
+      <Routes component={Home} path='home' />
     </Router>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
-const Home = () => {
+
+const Home = props => {
   return (
-    <h1> Hello This is Home page</h1>
+    <main className="d-flex flex-column justify-content-center ">
+      <section className="align-self-center text-center">
+        <h1> {"{...}"} </h1>
+        <h2> Hello Coder ! </h2>
+        <h3> Are you ready for the next challenge ?
+        <p> Take a Quiz </p>
+        </h3>
+      </section>
+      <section className="align-self-center text-center">
+        <ul className="list-unstyled">
+          <li> HTML <Button href="Quiz" > HTML</Button> </li>
+          <li> CSS  <Button href="./Quiz" > CSS</Button> </li>
+          <li> JavaScript <Button href="./Quiz"> JavaScript</Button> </li>
+        </ul>
+      </section>
+    </main>
   );
 };
 
