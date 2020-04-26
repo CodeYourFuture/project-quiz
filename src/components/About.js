17b9ee7c1ea86408f7a93c07c3e18fb512f83936
abdulkadiret <ethiogentelman@gmail.com>
2020-04-26 23:13:13 +0100

    Create about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React from 'react';
 
 const About = () => {
   return (
-    <h1> Hello This is About page</h1>
+    <main className="mx-5 my-3">
+      <h2 className="text-uppercase">
+        <strong>About CYF Quiz Project</strong>
+      </h2>
+      <p>Hello, and welcome to CYF quiz web app, so glad you're here.</p>
+      <p>
+        We are a group of graduates from CYF bootcamp, and decided to create
+        this application based on our experience as previous students to the
+        course, and as a graduates who are looking to give back to CYF
+        community. This app will help students to test their knowledge about
+        each given syllabuses, by taking each related quiz. With this app, we
+        aim to encourage our students to take new challenges, and build on the
+        confidence to try new thing every time. It is also a testament for us
+        the developers to present a valuable product that has been actively
+        benefiting the current students.
+      </p>
+      <h2 className="text-uppercase">
+        <strong>Meet the team</strong>
+      </h2>
+      <h6>The developers of this project are:</h6>
+      <ul>
+        <li>AbdulKadir Awel</li>
+        <li>Viktoriia Kosenko</li>
+        <li>Ahmed Merehil</li>
+        <li>Ali Sonmez</li>
+      </ul>
+    </main>
   );
 };
 
