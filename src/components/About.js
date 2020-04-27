2c8577591cad80b14381b5672c41e7ec6ee96deb
abdulkadiret <ethiogentelman@gmail.com>
2020-04-27 23:05:54 +0100

    Change main tag with Container & update paragraph text

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,30 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const About = () => {
   return (
-    <main className="mx-5 my-3">
-      <h2 className="text-uppercase">
-        <strong>About CYF Quiz Project</strong>
-      </h2>
-      <p>Hello, and welcome to CYF quiz web app, so glad you're here.</p>
+    <Container className="my-3">
+      <h1>About CYF Quiz Project</h1>
+      <p>Hello, and welcome to CYF Quiz. We're so glad you're here.</p>
       <p>
-        We are a group of graduates from CYF bootcamp, and decided to create
-        this application based on our experience as previous students to the
-        course, and as a graduates who are looking to give back to CYF
-        community. This app will help students to test their knowledge about
-        each given syllabuses, by taking each related quiz. With this app, we
-        aim to encourage our students to take new challenges, and build on the
-        confidence to try new thing every time. It is also a testament for us
-        the developers to present a valuable product that has been actively
-        benefiting the current students.
+        We are a group of graduates from CodeYourFuture who decided to create
+        CYF Quiz based on our experience as previous students to the course and
+        as a graduates who are looking to give back to the CYF community. CYF
+        Quiz will help students to test their knowledge on the various CYF
+        syllabus modules. With CYF Quiz, we aim to encourage our students to
+        take new challenges, and build their confidence to try new things every
+        time. It is also a testament for us, the developers, to present a
+        valuable product that benefits the current students.
       </p>
-      <h2 className="text-uppercase">
-        <strong>Meet the team</strong>
-      </h2>
-      <h6>The developers of this project are:</h6>
+      <h2>Meet the team</h2>
+      <p>The developers of this project are:</p>
       <ul>
         <li>AbdulKadir Awel</li>
         <li>Viktoriia Kosenko</li>
         <li>Ahmed Merehil</li>
         <li>Ali Sonmez</li>
       </ul>
-    </main>
+    </Container>
   );
 };
 
