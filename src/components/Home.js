38dd9401a014136313df5cdabe537f724d8d97c2
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-04-27 16:47:58 +0100

    Edit html file.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,15 @@ import { Button } from 'react-bootstrap';
 const Home = () => {
   return (
     <main className="text-center">
-      <section>
-        <h2>{"{ CYF Quiz }"}</h2>
-        <p>Hello Coder !<br />
-        Are you ready for the next challenge ?<br />
-        Take a quiz !</p>
-        <ul className="list-unstyled">
-          <li className="mb-3"><Button href="Quiz">HTML</Button></li>
-          <li className="mb-3"><Button href="Quiz">CSS</Button></li>
-          <li className="mb-3"><Button href="Quiz">JavaScript</Button></li>
-        </ul>
-      </section>
+      <h1>{"CYF Quiz"}</h1>
+      <p>Hello Coder !</p>
+      <p>Are you ready for the next challenge?</p>
+      <p>Take a quiz!</p>
+      <ul className="list-unstyled">
+        <li className="mb-1"><Button href="quiz">HTML</Button></li>
+        <li className="mb-1"><Button href="quiz">CSS</Button></li>
+        <li className="mb-1"><Button href="quiz">JavaScript</Button></li>
+      </ul>
     </main>
   );
 };
