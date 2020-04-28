5667e78638b5c731f134fb6dce8ae490ebdb6a8e
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-04-29 00:14:04 +0100

    Edit styling for home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,13 @@ const Home = () => {
   return (
     <main className="text-center">
       <h1>CYF Quiz</h1>
-      <p className="mb-0 py-0 h3 mt-4">Hello Coder !</p>
-      <p className="mb-0 py-0 h3">Are you ready for the next challenge?</p>
-      <p className="mb-0 py-0 h3">Take a quiz!</p>
+      <p className="mb-0 h3">Hello Coder !</p>
+      <p className="mb-0 h3">Are you ready for the next challenge?</p>
+      <p className="mb-0 h3">Take a quiz!</p>
       <ul className="list-unstyled my-4">
-        <li><Button className="mb-2 btn-block w-25 mx-auto font-weight-bold" href="quiz">HTML</Button></li>
-        <li><Button className="mb-2 btn-block w-25 mx-auto font-weight-bold" href="quiz">CSS</Button></li>
-        <li><Button className="mb-2 btn-block w-25 mx-auto font-weight-bold" href="quiz">JavaScript</Button></li>
+        <li><Button className="mb-2 px-4 font-weight-bold" href="quiz">HTML</Button></li>
+        <li><Button className="mb-2 px-4 font-weight-bold" href="quiz">CSS</Button></li>
+        <li><Button className="mb-2 px-4 font-weight-bold" href="quiz">JavaScript</Button></li>
       </ul>
     </main>
   );
