aa774c9177685f05d8bad2205e48fc7aeb3859aa
abdulkadiret <ethiogentelman@gmail.com>
2020-05-05 08:27:45 +0100

    Add navigation bar for all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import Footer from './components/Footer';
-
+import NavigationBar from './components/NavigationBar';
 import './App.scss';
 
 const App = () => {
   return (
     <Router>
+      <NavigationBar />
       <Routes />
       <Footer />
     </Router>
