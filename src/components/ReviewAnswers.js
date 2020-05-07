33e59d94a84ff487867c2f8c9dfcc0da768953f2
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-05-08 00:44:45 +0100

    Fix bootstrap styling

--- src/components/ReviewAnswers.js
+++ src/components/ReviewAnswers.js
@@ -10,47 +10,47 @@ const ReviewAnswers = ({ userAnswers, shuffledData }) => {
 
   return (
     <div>
-      <Container>
-        <Card>
-          <Card.Body>
-            <Card.Title className="text-center">Review answers</Card.Title>
-
-            <ol>
-              {shuffledData.map(question => {
-                return (
-                  <Card className="mr-2 mb-4">
-                    <Card.Body>
-                      <Card.Text>
-                        <Jumbotron>
-                          <li>{question.text}</li>
-                        </Jumbotron>
-                        <ul>
-                          {question.answers.map(answer => {
-                            return (
-                              <li
-                                className={
-                                  answer.isCorrect
-                                    ? 'text-success'
-                                    : userAnswers[question.id] === answer.id
-                                    ? 'text-danger'
-                                    : ''
-                                }
-                              >
-                                {answer.text}
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                );
-              })}
-            </ol>
-          </Card.Body>
-        </Card>
-      </Container>
-      <Button className="mr-2 mb-4" variant="secondary" href="quiz">
+      <Card>
+        <Card.Body>
+          <Card.Title className="text-center">Review answers</Card.Title>
+          <ol>
+            {shuffledData.map(question => {
+              return (
+                <Card className="mr-2 mb-4">
+                  <Card.Body>
+                    <Card.Text>
+                      <div
+                        className="p-4 mb-3"
+                        style={{ backgroundColor: 'lightGrey' }}
+                      >
+                        <li>{question.text}</li>
+                      </div>
+                      <ul>
+                        {question.answers.map(answer => {
+                          return (
+                            <li
+                              className={
+                                answer.isCorrect
+                                  ? 'text-success'
+                                  : userAnswers[question.id] === answer.id
+                                  ? 'text-danger'
+                                  : ''
+                              }
+                            >
+                              {answer.text}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </ol>
+        </Card.Body>
+      </Card>
+      <Button className="mt-4 mr-2 mb-4" variant="secondary" href="quiz">
         Try again
       </Button>
     </div>
