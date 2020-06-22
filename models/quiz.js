91159bba0d2ab604478a8c2b25c241872c97bf20
abdulkadiret <ethiogentelman@gmail.com>
2020-06-22 10:39:26 +0100

    Create QuizSchema & Quiz model

---
+++ models/quiz.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let QuizSchema = new Schema({
+  name: { type: String, required: true },
+  question: {
+    text: { type: String, required: true },
+    qType: { type: String, required: true },
+    answers: [
+      {
+        text: { type: String, required: true },
+        isCorrect: { type: Boolean, required: true },
+      },
+    ],
+  },
+});
+
+const Quiz = mongoose.model("Quiz", QuizSchema);
+
+module.exports = Quiz;
