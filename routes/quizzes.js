d04d084916fabd619a9368820b6874499d3f8fe7
abdulkadiret <ethiogentelman@gmail.com>
2020-06-22 14:49:45 +0100

    Create POST router/endpoint

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,15 +1,29 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Quiz = require("../models/quiz");
+const Quiz = require('../models/quiz');
 
 //Getting all questions
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const { quizName } = req.query;
   try {
     const quizQuestions = await Quiz.find({ name: quizName });
     res.status(200).json(quizQuestions);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+//Creating a question
+router.post('/', async (req, res) => {
+  const quiz = new Quiz({
+    name: req.body.name,
+    question: req.body.question,
+  });
+  try {
+    const newQuestion = await quiz.save();
+    res.status(201).json(newQuestion);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
