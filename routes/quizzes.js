eba9a0aafd17c8dcecec58ef20afb24529cfc90f
abdulkadiret <ethiogentelman@gmail.com>
2020-06-22 13:35:01 +0100

    Create GET route/endpoint

---
+++ routes/quizzes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Quiz = require("../models/quiz");
+
+//Getting all questions
+router.get("/", async (req, res) => {
+  const { quizName } = req.query;
+  try {
+    const quizQuestions = await Quiz.find({ name: quizName });
+    res.status(200).json(quizQuestions);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
