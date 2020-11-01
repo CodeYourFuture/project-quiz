ed63b25376a387bc8be61f22dc4eaf1152d4eea5
abdulkadiret <ethiogentelman@gmail.com>
2020-11-01 14:48:03 +0000

    Configer MONGODB_URI, .env file & add heroku-postbuild script

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const quizRouter = require('./routes/quizzes');
+const quizRoutes = require('./routes/quizzes');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
+db.once('connected', () => console.log('MongoDB connected'));
 db.on('error', error => console.error(error));
-db.once('open', () => console.log('Connected to database'));
 
 app.use(morgan('tiny'));
 app.use(express.json());
-app.use('/quizzes', quizRouter);
+app.use('/quizzes', quizRoutes);
 
-const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
