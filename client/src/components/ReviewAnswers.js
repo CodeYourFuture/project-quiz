ed2698477ef9868d8fa695afd54d85d3a6ce6156
abdulkadiret <ethiogentelman@gmail.com>
2020-11-01 15:44:58 +0000

    Style the ReviewAnswers component by adding ☐, ballot_box_with_check & ☒ marks

--- client/src/components/ReviewAnswers.js
+++ client/src/components/ReviewAnswers.js
@@ -1,13 +1,90 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Form, Container } from 'react-bootstrap';
 
 const getAnswersClass = (question, userAnswers, answer) => {
   if (answer.isCorrect) {
-    return 'text-success';
+    return 'bg-success text-white';
   } else if (userAnswers[question._id].includes(answer._id)) {
-    return 'text-danger';
+    return 'bg-danger text-white';
   } else {
-    return '';
+    return 'bg-light';
+  }
+};
+
+const getAnswersMark = (question, userAnswers, answer, selectedAnswers) => {
+  if (answer.isCorrect) {
+    if (selectedAnswers.includes(answer._id)) {
+      return (
+        <span
+          style={{
+            fontSize: '2.5rem',
+            color: '#383838',
+            position: 'relative',
+            background: 'none',
+            bottom: '-0.5rem',
+            left: '0.3rem',
+          }}
+        >
+          &#x2610;
+          <span
+            style={{
+              fontSize: '1.5rem',
+              color: '#383838',
+              position: 'absolute',
+              bottom: '0.1rem',
+              left: '0.4rem',
+            }}
+          >
+            &#x2713;
+          </span>
+        </span>
+      );
+    } else {
+      return (
+        <span
+          style={{
+            fontSize: '2.5rem',
+            color: '#383838',
+            position: 'relative',
+            background: 'none',
+            bottom: '-0.5rem',
+            left: '0.3rem',
+          }}
+        >
+          &#x2610;
+        </span>
+      );
+    }
+  } else if (userAnswers[question._id].includes(answer._id)) {
+    return (
+      <span
+        style={{
+          fontSize: '2.5rem',
+          color: '#383838',
+          position: 'relative',
+          background: 'none',
+          bottom: '-0.5rem',
+          left: '0.3rem',
+        }}
+      >
+        &#9746;
+      </span>
+    );
+  } else {
+    return (
+      <span
+        style={{
+          fontSize: '2.5rem',
+          color: '#383838',
+          position: 'relative',
+          background: 'none',
+          bottom: '-0.5rem',
+          left: '0.3rem',
+        }}
+      >
+        &#x2610;
+      </span>
+    );
   }
 };
 
@@ -19,31 +96,56 @@ const ReviewAnswers = ({
   numOfQuestions,
 }) => {
   return (
-    <div>
+    <Container>
       <p>
         Your score is: {score} / {numOfQuestions}
       </p>
-      {questions.map(question => (
-        <Card key={question._id} className="mb-4">
-          <Card.Body>
-            <Card.Title>{question.text}</Card.Title>
-            <ul className="mb-0">
-              {question.answers.map(answer => (
-                <li
-                  key={answer._id}
-                  className={getAnswersClass(question, userAnswers, answer)}
-                >
-                  {answer.text}
-                </li>
-              ))}
-            </ul>
-          </Card.Body>
-        </Card>
-      ))}
-      <Button className="mb-4" variant="primary" href={quizName}>
-        Try again
-      </Button>
-    </div>
+      <Form>
+        <fieldset disabled>
+          {questions.map(question => (
+            <Card key={question._id} className="mb-4">
+              <Card.Header as="h5">{question.text}</Card.Header>
+              <Card.Body className="bg-white">
+                <Form.Group className="bg-light" key={question._id}>
+                  <ul className="list-unstyled">
+                    {question.answers.map(answer => {
+                      const selectedAnswers = userAnswers[question._id];
+                      return (
+                        <li
+                          style={{
+                            borderBottom: '1px solid white',
+                            position: 'relative',
+                          }}
+                          key={answer._id}
+                          className={getAnswersClass(
+                            question,
+                            userAnswers,
+                            answer,
+                          )}
+                        >
+                          {getAnswersMark(
+                            question,
+                            userAnswers,
+                            answer,
+                            selectedAnswers,
+                          )}
+                          <label className="review-answers px-2">
+                            {answer.text}
+                          </label>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </Form.Group>
+              </Card.Body>
+            </Card>
+          ))}
+        </fieldset>
+        <Button className="mb-4" variant="primary" href={quizName}>
+          Try again
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
